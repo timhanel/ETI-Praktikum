13a96e931c4710f9db0c57afa2e1deba4e460aa7
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-02-08 14:45:49 +0100

    reformat code

--- Praktikum ETI/P3_GPU/openMPOffload.cpp
+++ Praktikum ETI/P3_GPU/openMPOffload.cpp
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <math.h>
 
-#define SIZE 1024
+#define SIZE 2048
 #define NUMTEAMS 64
 
 void initOutput(double *input) {
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
 
     matmuljki(a, b, test);
 
-    initOutput(c);
+    //initOutput(c);
     printf("\n");
 #pragma omp target data map(to:a[:SIZE*SIZE]) map(to: b[:SIZE*SIZE]) map(from: c[:SIZE*SIZE])
     {
